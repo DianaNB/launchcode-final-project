77692bd01bc354379c67c72cbced4de2d2c6d7bc
Adrienne <akoeck101@yahoo.com>
2019-11-30 16:08:44 -0500

    added Snake enum + Post class, changes to PostController, got create post/list view to work

---
+++ src/main/java/com/launchcode/adrienne/FinalProjectTest/models/Snake.java
@@ -0,0 +1,41 @@
+package com.launchcode.adrienne.FinalProjectTest.models;
+
+public enum Snake {
+    BALL_PYTHON("Ball Python", "Python"),
+    RETICULATED_PYTHON("Reticulated Python", "Python"),
+    BURMESE_PYTHON("Burmese Python","Python"),
+    SAND_BOA("Sand Boa","Boa"),
+    BOA_CONSTRICTOR("Boa Constrictor","Boa"),
+    AMAZON_TREE_BOA("Amazon Tree Boa","Boa"),
+    EMERALD_GREEN_TREE_BOA("Emerald Green Tree Boa","Boa"),
+    CORN_SNAKE("Corn Snake","Colubrid"),
+    RAT_SNAKE("Rat Snake","Colubrid"),
+    INDIGO_SNAKE("Indigo Snake","Colubrid"),
+    EGG_EATING_SNAKE("Egg Eating Snake","Colubrid");
+
+
+    String name;
+    String category;
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getCategory() {
+        return category;
+    }
+
+    Snake(String name, String category) {
+        this.name = name;
+        this.category = category;
+
+
+//        for (Snake : Snake.values()) {
+
+//            add the snake name to your list
+//        }
+
+    }
+}
