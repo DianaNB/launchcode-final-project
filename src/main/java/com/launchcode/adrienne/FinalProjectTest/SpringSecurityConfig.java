4d05413aacdedfe9bb876693559ade901e6dabbf
Adrienne <akoeck101@yahoo.com>
2019-12-09 18:57:52 -0500

    Finalized authentication

--- src/main/java/com/launchcode/adrienne/FinalProjectTest/SpringSecurityConfig.java
+++ src/main/java/com/launchcode/adrienne/FinalProjectTest/SpringSecurityConfig.java
@@ -5,17 +5,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.handler.MappedInterceptor;
 import org.springframework.web.servlet.resource.GzipResourceResolver;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
-public class SpringSecurityConfig {
-
+public class SpringSecurityConfig implements WebMvcConfigurer{
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor( new MappedInterceptor(new String[]{"/**"}, new String[]{"/images/**"}, authentication()) );
+    }
     @Bean
-    public MappedInterceptor authentication() {
-        return new MappedInterceptor(new String[] {"/**"},new String[]{"/images/**"}, new Authentication());
+    public Authentication authentication() {
+        return new Authentication();
     }
     }
 
